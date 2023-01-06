afd2b6de4d6171d412b41ee9984f74d1be3c0723
Jorge Vergara <jorge@jorgevergara.co>
2023-01-06 07:49:23 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'egghead-task-manager',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
