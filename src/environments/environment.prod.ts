f56c1fbb11999489047dd27316c203c0112ad274
Jorge Vergara <jorge@jorgevergara.co>
2023-01-06 07:58:25 -0500

    chore: uses the ng add schematic to initialize Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'egghead-task-list',
+    appId: '1:746061257985:web:f270de70660d8d3a81cf00',
+    storageBucket: 'egghead-task-list.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBD29P0AuovkEBX-83HnmEMRWk23vfaet4',
+    authDomain: 'egghead-task-list.firebaseapp.com',
+    messagingSenderId: '746061257985',
+  },
   production: true
 };
