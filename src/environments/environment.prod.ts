a25c5e44c7d3afa725e678d59cb754ef039b1547
Jorge Vergara <jorge@jorgevergara.co>
2023-01-07 13:16:43 -0500

    chore: moves firebase projects

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'egghead-task-list',
-    appId: '1:746061257985:web:f270de70660d8d3a81cf00',
-    storageBucket: 'egghead-task-list.appspot.com',
+    projectId: 'task-list-94997',
+    appId: '1:460499010450:web:6b731ad44a4959b271b74b',
+    storageBucket: 'task-list-94997.appspot.com',
     locationId: 'us-central',
-    apiKey: 'AIzaSyBD29P0AuovkEBX-83HnmEMRWk23vfaet4',
-    authDomain: 'egghead-task-list.firebaseapp.com',
-    messagingSenderId: '746061257985',
+    apiKey: 'AIzaSyBkTZ7sZKl9I-xQIPR2mcbF6FfM7cdxO58',
+    authDomain: 'task-list-94997.firebaseapp.com',
+    messagingSenderId: '460499010450',
   },
-  production: true
+  production: true,
 };
