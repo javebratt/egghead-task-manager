ee5ffda2155863ccf7c8e4f23686a5bd737f67bf
Jorge Vergara <jorge@jorgevergara.co>
2023-01-07 15:00:49 -0500

    chore: fetches the details of a task from Firestore

--- src/app/tasks/task/task.page.ts
+++ src/app/tasks/task/task.page.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { doc, docData, Firestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Task, TaskStatus } from '../tasks.models';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.page.html',
   styleUrls: ['./task.page.scss'],
 })
-export class TaskPage implements OnInit {
+export class TaskPage {
+  newTask: Task = {
+    id: '',
+    title: 'New Ticket',
+    description: '',
+    status: TaskStatus.TODO,
+    createdAt: 0,
+    assignee: '',
+    attachments: [],
+  };
 
-  constructor() { }
+  task$: Observable<Task> = this.route.params.pipe(
+    switchMap((params) => {
+      const taskId: string = params['taskId'];
+      return taskId === 'new'
+        ? of(this.newTask)
+        : (docData(doc(this.firestore, `tasks/${taskId}`)) as Observable<Task>);
+    })
+  );
 
-  ngOnInit() {
-  }
-
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly firestore: Firestore
+  ) {}
 }
