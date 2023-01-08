eb2e6e278f29a26fa9751fbac32bd565a9eafded
Jorge Vergara <jorge@jorgevergara.co>
2023-01-07 19:39:37 -0500

    feat: adds the detail page for a ticket

--- src/app/tasks/task/task.page.ts
+++ src/app/tasks/task/task.page.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { doc, docData, Firestore } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { Task, TaskStatus } from '../tasks.models';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { Task } from '../tasks.models';
 
 @Component({
   selector: 'app-task',
@@ -11,23 +11,12 @@ import { Task, TaskStatus } from '../tasks.models';
   styleUrls: ['./task.page.scss'],
 })
 export class TaskPage {
-  newTask: Task = {
-    id: '',
-    title: 'New Ticket',
-    description: '',
-    status: TaskStatus.TODO,
-    createdAt: 0,
-    assignee: '',
-    attachments: [],
-  };
-
   task$: Observable<Task> = this.route.params.pipe(
-    switchMap((params) => {
-      const taskId: string = params['taskId'];
-      return taskId === 'new'
-        ? of(this.newTask)
-        : (docData(doc(this.firestore, `tasks/${taskId}`)) as Observable<Task>);
-    })
+    map((params) => params['taskId'] as string),
+    switchMap(
+      (taskId) =>
+        docData(doc(this.firestore, `tasks/${taskId}`)) as Observable<Task>
+    )
   );
 
   constructor(
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -6,11 +6,18 @@ import { IonicModule } from '@ionic/angular';
 
 import { TasksPageRoutingModule } from './tasks-routing.module';
 
+import { FileSizePipeModule } from '../shared/pipes/file-size.pipe.module';
 import { TaskPage } from './task/task.page';
 import { TasksPage } from './tasks.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, TasksPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    TasksPageRoutingModule,
+    FileSizePipeModule,
+  ],
   declarations: [TasksPage, TaskPage],
 })
 export class TasksPageModule {}
